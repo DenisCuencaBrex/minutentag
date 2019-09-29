9182815a7aa93e793e807136fc995f7f89140b7a
Denis Cuenca Brex <deniscbrex.dev@gmail.com>
2019-09-28 21:43:42 -0300

    fixed css and js source error

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,11 +2,11 @@ const slideImages = document.querySelector(".slide");
 let x = 0;
 
 let images = [
-  "/projects/minuten/images/ann_taylor_logo-min.jpg",
-  "/projects/minuten/images/childrensplace_logo-min.jpg",
-  "/projects/minuten/images/loft_logo-min.jpg",
-  "/projects/minuten/images/lou_and_grey_logo-min.jpg",
-  "/projects/minuten/images/sapient_razorfish_logo-min.jpg",
+  "../../projects/minuten/images/ann_taylor_logo-min.jpg",
+  "../../projects/minuten/images/childrensplace_logo-min.jpg",
+  "../../projects/minuten/images/loft_logo-min.jpg",
+  "../../projects/minuten/images/lou_and_grey_logo-min.jpg",
+  "../../projects/minuten/images/sapient_razorfish_logo-min.jpg",
 ];
 
 
