eb2c7fd841b057b83a39456be76ee84febb1a751
Denis Cuenca Brex <deniscbrex.dev@gmail.com>
2019-09-28 23:29:04 -0300

    added difuse when click to href menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,6 +4,7 @@ const links = document.querySelectorAll(".nav-links li");
 const hamburgerToggle = document.querySelector(".toggle");
 const navbarBrand = document.querySelector(".navbar-brand")
 const navText = document.querySelector(".txt");
+const ref = document.querySelectorAll(".ref");
 
  hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
@@ -14,3 +15,15 @@ const navText = document.querySelector(".txt");
     link.classList.toggle("fade");
   });
 });
+
+for (let i = 0; i < ref.length; i++) {
+  ref[i].addEventListener("click", function() {
+    navLinks.classList.toggle("open");
+    hamburgerToggle.classList.toggle("active");
+    navbarBrand.classList.toggle("active");
+    navText.classList.toggle("inactive");
+    links.forEach(link => {
+      link.classList.toggle("fade");
+    });
+  });
+}
